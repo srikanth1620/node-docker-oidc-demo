43f915782565b6d6ab8d670ec1a60aba0a1d016e
srikanthsrinivasan <srikanth.srinivasa@gmail.com>
2026-04-12 20:32:58 -0500

    testing

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Azure App Service running Docker!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
